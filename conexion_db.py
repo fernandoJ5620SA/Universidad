3a5c81d4a310a2afdb322a60bdecf9d198e7884c
CristianGutierrezHdz <24gutierrez.cristian@gmail.com>
2024-04-18 18:11:43 -0600

    epaaa

---
+++ conexion_db.py
@@ -0,0 +1,13 @@
+
+# Importando Libreria mysql.connector para conectar Python con MySQL
+# pip install mysql-connector-python
+
+import mysql.connector
+
+def connectionBD():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="1423#Ghz1233",
+        database="Universidad"
+    )
--- config.py
+++
@@ -1,9 +0,0 @@
-# Aquí puedes colocar la configuración de tu aplicación Flask, como la configuración de la base de datos.
-
-
-class Config:
-    SECRET_KEY = "your_secret_key_here"
-    MYSQL_HOST = "localhost"
-    MYSQL_USER = "root"
-    MYSQL_PASSWORD = ""
-    MYSQL_DB = "flask_crud"
