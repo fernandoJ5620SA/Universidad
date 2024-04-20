ce3f6ed716a9aeb886c9d2a595585c8e2f19e257
CristianGutierrezHdz <24gutierrez.cristian@gmail.com>
2024-04-19 18:03:03 -0600

    casi nada apa

--- src/routes/main.py
+++ src/routes/main.py
@@ -9,14 +9,15 @@ main_bp = Blueprint("main", __name__, template_folder="../views")
 
 # Definir rutas dentro del blueprint
 
+@main_bp.route("/")
+def index():
+    return render_template("layouts/dashboard_alumnos.html")
+    
 @main_bp.route("/index")
 def alumnos_materias():
     materias_alumn = src.controllers.alumnos.MateriasController.obtener_materias()
     return render_template("alumnos/materias_test.html", materias=materias_alumn)
 
-@main_bp.route("/")
-def index():
-    return render_template("layouts/dashboard_alumnos.html")
 
 @main_bp.route('/home', methods=['POST'])
 def header():
