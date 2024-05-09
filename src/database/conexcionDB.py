628d8e24c621819d977d737b058d49dcaf544388
josegilbertoguzmangutierrez <josegilbertoguzmangutierrez@outlook.com>
2024-05-08 19:26:57 -0600

    env

--- src/database/conexcionDB.py
+++ src/database/conexcionDB.py
@@ -1,12 +1,20 @@
 # Importando Libreria mysql.connector para conectar Python con MySQL
 # pip install mysql-connector-python
-
+import os
 import mysql.connector
+from dotenv import load_dotenv
+
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
 
 
 def connectionBD():
     mydb = mysql.connector.connect(
-        host="localhost", port=3306, user="root", passwd="zxcvbnm,", database="universidad"
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        user=os.getenv("DB_USER"),
+        passwd=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
     )
 
     return mydb
