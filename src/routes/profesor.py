deccc37c9ad9644a385c85c72cace7297b0e87bf
CedrickMQ <112716158+CedrickMQ@users.noreply.github.com>
2024-05-08 20:03:56 -0600

    profesores vistas

--- src/routes/profesor.py
+++ src/routes/profesor.py
@@ -11,9 +11,24 @@ profesor_bp = Blueprint("profesor", __name__, template_folder="../views")
 # Definir rutas dentro del blueprint
 
 @profesor_bp.route("/profesores")
-def index():
+def Inicio():
     return render_template("Profesor/profesor.html")
-    
 
+@profesor_bp.route("/2")
+def V2():
+    return render_template("Profesor/Vista2.html")
+    
+@profesor_bp.route("/3")
+def V3():
+    return render_template("Profesor/Vista3.html")
+    
+@profesor_bp.route("/4")
+def V4():
+    return render_template("Profesor/Vista4.html")
+    
+@profesor_bp.route("/5")
+def V5():
+    return render_template("Profesor/Vista5.html")
+    
 
 
