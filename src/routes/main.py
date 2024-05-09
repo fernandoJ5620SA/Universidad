be0656d6a05fe4327b0269516d6099df1d0544d3
CristianGutierrezHdz <24gutierrez.cristian@gmail.com>
2024-05-08 19:12:04 -0600

    coomit 34

--- src/routes/main.py
+++ src/routes/main.py
@@ -13,12 +13,6 @@ main_bp = Blueprint("main", __name__, template_folder="../views")
 def index():
     return render_template("login.html")
 #layouts/dashboard_alumnos.html
-    
-@main_bp.route("/alumnos")
-def alumnos_materias():
-    materias_alumn = src.controllers.alumnos.MateriasController.obtener_materias()
-    return render_template("alumnos/materias_test.html", materias=materias_alumn)
-
 
 @main_bp.route('/home', methods=['POST'])
 def header():
