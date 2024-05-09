6a0e5697ca4704c8c99c10d1cbc16d7584ee8eab
Fernando S <fer22_jesus@outlook.com>
2024-05-08 19:06:09 -0600

    ora

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask import Flask
 from src.routes.main import main_bp
 from src.routes.admin import admin_bp
 from src.routes.profesor import profesor_bp
-from src.routes.alumnos import alumnos_bp
 from src.routes.auth import auth_bp
 
 # Crear una instancia de la aplicación Flask
@@ -16,7 +15,6 @@ app.register_blueprint(main_bp)
 app.register_blueprint(admin_bp)
 app.register_blueprint(auth_bp)
 app.register_blueprint(profesor_bp)
-app.register_blueprint(alumnos_bp)
 
 
 
