b9ba6144ec89a41c9bf98dffdf7bdc5cc1bae5d0
BrayanUlises <brayan.gonzalez35@unach.mx>
2024-05-08 20:03:58 -0600

    jijija

--- src/routes/alumnos.py
+++ src/routes/alumnos.py
@@ -7,21 +7,21 @@ alumnos_bp = Blueprint("alumnos", __name__, template_folder="../views")
 # Definir rutas dentro del blueprint
 
 @alumnos_bp.route("/alumnos")
-def index():
+def alumnos():
     return render_template("alumnos/inicio.html")
     
 @alumnos_bp.route("/alumnos/materias")
-def index():
+def alumnos_materias():
     return render_template("alumnos/materias.html")
 
 @alumnos_bp.route("/alumnos/Kardex")
-def index():
+def alumnos_kardex():
     return render_template("alumnos/Kardex.html")
 
 @alumnos_bp.route("/alumnos/Horarios")
-def index():
+def alumnos_horarios():
     return render_template("alumnos/Horarios.html")
 
 @alumnos_bp.route("/alumnos/Historial")
-def index():
+def alumnos_historial():
     return render_template("alumnos/Historial.html")    
