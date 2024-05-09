707396681a62777d79040ff53241ddaaf2f04ad5
CristianGutierrezHdz <24gutierrez.cristian@gmail.com>
2024-05-08 19:17:21 -0600

    commit gosht

--- src/routes/main.py
+++ src/routes/main.py
@@ -9,11 +9,6 @@ main_bp = Blueprint("main", __name__, template_folder="../views")
 
 # Definir rutas dentro del blueprint
 
-@main_bp.route("/")
-def index():
-    return render_template("Auth/login.html")
-#layouts/dashboard_alumnos.html
-
 @main_bp.route('/home', methods=['POST'])
 def header():
     return render_template('header.html')
